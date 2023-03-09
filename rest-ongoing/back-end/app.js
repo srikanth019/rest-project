6b8123787301c672dd35d14fd3e762714c785441
srikanth019 <srikanth.golla@brainvire.com>
2023-03-09 15:47:50 +0530

    Storing Posts in the database (378)

--- rest-ongoing/back-end/app.js
+++ rest-ongoing/back-end/app.js
@@ -1,5 +1,7 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
@@ -7,6 +9,8 @@ const app = express();
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -17,6 +21,14 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080, (req,res,next) => {
-    console.log("Server running");
-});
+mongoose.connect(
+    'mongodb+srv://srikanth19:1858260338@cluster0.5sgelp9.mongodb.net/posts?retryWrites=true&w=majority'
+)
+.then(result => {
+    app.listen(8080, (req,res,next) => {
+        console.log("Server running");
+    });
+})
+.catch(err => {
+    console.log(err);
+})
--- rest-ongoing/back-end/controllers/feed.js
+++ rest-ongoing/back-end/controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -26,16 +28,24 @@ exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
   // Create post in db
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: { 
-      _id: new Date().toISOString(), 
-      title: title, 
-      content: content,
-      creator: {
-        name: 'Srikanth'
-      },
-      createdAt: new Date()
-    }
+
+  const post = new Post({
+    title: title, 
+    content: content,
+    creator: {
+      name: 'Srikanth'  
+    },
+    imageUrl: 'images/hdp.jpg'
   });
+
+  post.save()
+  .then(result => {
+    // console.log(result);
+    res.status(201).json({
+      message: 'Post created successfully!',
+      post: result
+    });
+  }).catch(err => {
+    console.log(err);
+  })
 };
