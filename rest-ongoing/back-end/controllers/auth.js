c360c11b17e47158509962254d9c755b880c58fc
srikanth019 <srikanth.golla@brainvire.com>
2023-03-10 17:30:10 +0530

    Adding authorization to all routs  (393)

--- rest-ongoing/back-end/controllers/auth.js
+++ rest-ongoing/back-end/controllers/auth.js
@@ -101,7 +101,7 @@ exports.login = (req, res, next) => {
             email: loadedUser.email,
             userId: loadedUser._id.toString()
           },
-          'secret',
+          'secretMesssage',
           { expiresIn: '1h' }
         );
         res.status(200).json({ token: token, userId: loadedUser._id.toString() });
--- rest-ongoing/back-end/routes/feed.js
+++ rest-ongoing/back-end/routes/feed.js
@@ -1,27 +1,31 @@
 const express = require('express');
-const {body} = require('express-validator')
+const {body} = require('express-validator');
+const isAuth = require('../middleware/is-auth')
 
 const feedController = require('../controllers/feed');
 
 const router = express.Router();
 
 // GET /feed/posts
-router.get('/posts', feedController.getPosts);
+router.get('/posts',isAuth, feedController.getPosts);
 
 // POST /feed/post
-router.post('/post',[
-    body('title')
-        .trim()
-        .isLength({min: 5}),
-    body('content')
-        .trim()
-        .isLength({min: 5})
+router.post('/post',
+    isAuth,
+    [
+        body('title')
+            .trim()
+            .isLength({min: 5}),
+        body('content')
+            .trim()
+            .isLength({min: 5})
 
-], feedController.createPost);
+    ], feedController.createPost
+);
 
-router.get('/post/:postId', feedController.getPost);
+router.get('/post/:postId',isAuth ,feedController.getPost);
 
-router.put('/post/:postId',[
+router.put('/post/:postId',isAuth ,[
     body('title')
         .trim()
         .isLength({min: 5}),
@@ -31,6 +35,6 @@ router.put('/post/:postId',[
 
 ],feedController.updatePost);
 
-router.delete('/post/:postId', feedController.deletePost);
+router.delete('/post/:postId',isAuth ,feedController.deletePost);
 
 module.exports = router;
